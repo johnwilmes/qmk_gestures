ed182472b24a24a2b8ea7a979560c329fccbe04b
John Wilmes <jw@johnwilmes.name>
2026-04-11 20:09:55 -0500

    fix introspection and combo missing symbols

--- gestures/introspection.h
+++ gestures/introspection.h
@@ -5,5 +5,8 @@
  * satisfies that requirement so users don't need to define keymaps themselves.
  * A user-supplied keymaps definition (strong symbol) will override this.
  */
+#include <stdint.h>
+#include "progmem.h"
+
 __attribute__((weak)) const uint16_t PROGMEM
     keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = {{0}}};
--- gestures/types/combo.h
+++ gestures/types/combo.h
@@ -3,6 +3,11 @@
 #include "gesture_api.h"
 #include "progmem.h"
 
+#ifndef COUNT_ARGS
+#define COUNT_ARGS(...) COUNT_ARGS_IMPL(__VA_ARGS__, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define COUNT_ARGS_IMPL(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, N, ...) N
+#endif
+
 typedef uint8_t combo_active_mask_t;
 
 /* Pointer to a PROGMEM array of key indices (user-defined dense positions).
